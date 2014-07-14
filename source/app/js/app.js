ef1440af85844a26e67e634473ca46762d6d1fb6
sriram <sriram@PRINHYLTPAP0181.local>
2014-07-14 02:00:21 -0700

    Base Version Of StickyNotes Application

---
+++ source/app/js/app.js
@@ -0,0 +1,9 @@
+var app = angular.module("StickyNotesApplication", []).config(function ($routeProvider) {
+
+    $routeProvider.when("/", {
+        templateUrl: "app/view/page.html",
+        controller: ""
+    });
+
+
+});
