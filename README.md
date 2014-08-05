d0f29ac55c3db292faf67afbf9b8ce88db4aca19
Sowmya Narayan <sowmyakavi@gmail.com>
2014-08-05 06:31:02 -0700

    Basic Functionality and Layout

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-AngularJS - StickyNotes
+StickyNotes
+
+
+This Application is replica of keep.google.com implemented using Angularjs
+
+Inorder to run the application, pre requesties are:
+
+		1. Ruby 
+
+		  type ruby -v in terminal 
+		  if you dont have one install 
+
+		2. Install MiddleMan App
+		   
+		   gem install middleman 
+
+    3. Clone the project and get into directory 
+
+       bundle install
+
+    4. start middleman server 
+
+       bundle exec middleman
+
+    5. In browser go to -->  http://0.0.0.0:4567
+
--- source/app/js/app.js
+++ source/app/js/app.js
@@ -1,9 +1,55 @@
-var app = angular.module("StickyNotesApplication", []).config(function ($routeProvider) {
+var app = angular.module("StickyNotesApplication", ['ngRoute']).config(function ($routeProvider) {
 
     $routeProvider.when("/", {
-        templateUrl: "app/view/page.html",
-        controller: ""
+        templateUrl: "app/view/notes.html",
+        controller: "notes"
     });
 
+
+});
+
+app.directive('noteInterface', function(){
+  return {
+    restrict: 'AE',
+    templateUrl: "app/view/templates/noteInterface.html",
+  }
+});
+
+app.controller("commonComponents", function ($scope) {
+	$scope.showHideSideBar = function(){
+		$scope.showSideBar = $scope.showSideBar ? false : true;
+	}
+
+
+})
+.controller("notes", function ($scope) {
+	$scope.addListNote = false;
+	$scope.newList = [{value: '', selected: false}];
+	$scope.notes = JSON.parse(localStorage.getItem("StickyNotesApplication")) || [];
+	//$scope.notes = [{type: 'Note', data: 'test the note', title: 'Note'}, {type: 'ListNote', data: [{value: 'item1', selected: true}, {value: 'item2', selected: false}, {value: 'item3', selected: false}], title: 'List Note'}/*, {type: 'Image', data: 'image note test', title: 'Image Note', image: ''}*/];
+
+	$scope.createNote = function(){
+		if($scope.addListNote){
+			$scope.newList.pop();
+			$scope.notes.push({type: 'ListNote', data: $scope.newList, title: $scope.title});
+		}else{
+			$scope.notes.push({type: 'Note', data: $scope.addNote, title: $scope.title});
+		}
+		localStorage.setItem("StickyNotesApplication", JSON.stringify($scope.notes));
+		$scope.newList = [{value: '', selected: false}];
+		$scope.addNote = "";
+		$scope.title = "";
+
+	}
+
+	$scope.toggleList = function(){
+		$scope.addListNote = $scope.addListNote ? false : true;
+	}
+
+	$scope.$watch('newList[newList.length-1].value', function(val) {
+		  if(val != ''){
+       $scope.newList.push({value: '', selected: false});
+		  }
+   });
 
 });
