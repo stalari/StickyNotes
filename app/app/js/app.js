16e0bc29f02e02a3883943f97d2cda08089b8a8e
Sowmya Narayan <sowmyakavi@gmail.com>
2014-08-07 04:16:40 -0700

    updated to support the upload the images ..

--- app/app/js/app.js
+++ app/app/js/app.js
@@ -47,6 +47,8 @@ app.controller("commonComponents", function ($scope, $rootScope) {
 		if($scope.addListNote){
 			$scope.newList.pop();
 			$scope.editIndex === undefined ? $scope.notes.push({type: 'ListNote', data: $scope.newList, title: $scope.title, color: $scope.color}) : $scope.notes[$scope.editIndex] = {type: 'ListNote', data: $scope.newList, title: $scope.title, color: $scope.color};
+		}else if($scope.addImageNote){
+			$scope.editIndex === undefined ? $scope.notes.push({type: 'ImageNote', data: $scope.addNote, title: $scope.title, color: $scope.color, img: $('img').attr('src')}) : $scope.notes[$scope.editIndex] = {type: 'ImageNote', data: $scope.addNote, title: $scope.title, color: $scope.color, img: $('img').attr('src')};
 		}else{
 			$scope.editIndex === undefined ? $scope.notes.push({type: 'Note', data: $scope.addNote, title: $scope.title, color: $scope.color}) : $scope.notes[$scope.editIndex] = {type: 'Note', data: $scope.addNote, title: $scope.title, color: $scope.color};
 		}
@@ -61,6 +63,7 @@ app.controller("commonComponents", function ($scope, $rootScope) {
 		$scope.title = "";
 		$scope.editIndex = undefined;
 		$scope.addListNote = false;
+		$scope.addImageNote = false;
 		$scope.color = 'white';
 	}
 
@@ -69,6 +72,12 @@ app.controller("commonComponents", function ($scope, $rootScope) {
 		if($scope.notes[index].type === 'Note'){
 			$scope.newList = [{value: '', selected: false}];
 			$scope.addNote = $scope.notes[index].data;
+		}else if($scope.notes[index].type === 'ImageNote'){
+			$scope.newList = [{value: '', selected: false}];
+			$scope.addNote = $scope.notes[index].data;
+			$scope.noteImg = $scope.notes[index].img;
+			$scope.addListNote = false;
+			$scope.addImageNote = true;
 		}else{
 			$scope.addListNote = true;
 			$scope.newList = $scope.notes[index].data;
@@ -79,6 +88,11 @@ app.controller("commonComponents", function ($scope, $rootScope) {
 
 	$scope.toggleList = function(){
 		$scope.addListNote = $scope.addListNote ? false : true;
+		$scope.addImageNote = false;
+	}
+
+	$scope.toggleImage = function(){
+	  $scope.addImageNote = true; 
 	}
 
 	$scope.$watch('newList[newList.length-1].value', function(val) {
